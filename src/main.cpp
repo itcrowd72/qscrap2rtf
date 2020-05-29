1ffdb54888a981f1eb04441dc5ad7ae2f238ee64
Andrei Ivnitskii <ivnish.dev@gmail.com>
2020-05-29 12:02:16 +0500

    Add scrap2rtf check before launch the application

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <QTranslator>
 #include <QLocale>
 #include "helpers.h"
+#include <QMessageBox>
 
 QStringList szScrapNames;       // List of scrap names to convert (Global variable)
 bool bRemoveOriginal;           // Remove original scrap? (Global variable)
@@ -26,11 +27,11 @@ int main(int argc, char *argv[]) {
     }
   #else
   #ifdef Q_OS_WIN32
-    szTranslateName = qApp->applicationDirPath()+"\\locales\\" + QLocale::system().name() + "\\qscrap2rtf.qm";
+    szTranslateName = qApp->applicationDirPath() + "\\locales\\" + QLocale::system().name() + "\\qscrap2rtf.qm";
 
     // If translate not found try "QLocale::system().name().left(2)"
     if (!FileExists(szTranslateName)) {
-      szTranslateName = qApp->applicationDirPath()+"\\locales\\" + QLocale::system().name().left(2) + "\\qscrap2rtf.qm";
+      szTranslateName = qApp->applicationDirPath() + "\\locales\\" + QLocale::system().name().left(2) + "\\qscrap2rtf.qm";
     }
   #endif
   #endif
@@ -42,6 +43,32 @@ int main(int argc, char *argv[]) {
 
   Wizard w;
   w.show();
+
+  #ifdef Q_OS_LINUX
+    // Check scrap2rtf
+    if (!FileExists("/usr/bin/scrap2rtf")) {
+      QMessageBox msgBox;
+      msgBox.setText(QObject::tr("scrap2rtf not found. Please install scrap2rtf first."));
+      msgBox.setIcon(QMessageBox::Critical);
+
+      if (msgBox.exec() == QMessageBox::Ok) {
+        return 1;
+      }
+    }
+  #else
+  #ifdef Q_OS_WIN32
+    // Check scrap2rtf
+    if (!FileExists(qApp->applicationDirPath() + "\\bin\\scrap2rtf.exe") {
+      QMessageBox msgBox;
+      msgBox.setText(QObject::tr("scrap2rtf not found. Please reinstall application."));
+      msgBox.setIcon(QMessageBox::Critical);
+
+      if (msgBox.exec() == QMessageBox::Ok) {
+        return 1;
+      }
+    }
+  #endif
+  #endif
 
   return a.exec();
 }
